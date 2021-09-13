047c09f6628f295715ae0b52a5a43ad32fe1e526
EllenKC01 <ellen.k.cochran@gmail.com>
2021-09-13 13:01:12 -0400

    class edits

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@ var time = new Date().toLocaleTimeString();
 console.log(day);
 now = "Today is " + day + ".";
 console.log(now);
-console.log(time);
 currentTime = "Current time : " + time;
+console.log(currentTime);
 
 var todayIs = document.getElementById("answer1").innerHTML = now + "<br />" + currentTime;
 
@@ -41,37 +41,45 @@ var dateList = document.getElementById("answer2").innerHTML = datestring1 + "<br
 /* 3.--------
 
 */
+var code = document.getElementById("body").innerHTML;
 
 function myFunction() {
-    var code = document.getElementById("body").innerHTML;
+    // = document.documentElement.innerHTML;
     document.getElementById("answer3").innerText = code;
 }
+
+console.log(code);
 
 /* 4.--------
 
 */
 //
 
-paper.install(window);
-paper.setup(document.getElementById('main-canvas'));
+$(document).ready(function() { 'use strict';
+    console.log('main.js loaded');
 
-var pen = new Tool();
 
-var c = Shape.Circle(200, 200, 80);
-c.fillColor = 'black';
-var text = new PointText(200, 200);
-text.justification = 'center';
-text.fillColor = 'white';
-text.fontSize = 20;
-text.content = 'hello world';
+    paper.install(window);
+    paper.setup(document.getElementById('main-canvas'));
 
-tool.onMouseDown = function(event) {
-    var c = Shape.Circle(event.point, 20);
-    c.fillColor = 'green';
-};
+    var tool = new Tool();
 
-paper.view.draw();
+    var c = Shape.Circle(200, 200, 80);
+    c.fillColor = 'black';
+    var text = new PointText(200, 200);
+    text.justification = 'center';
+    text.fillColor = 'white';
+    text.fontSize = 20;
+    text.content = 'hello world';
 
+    tool.onMouseDown = function(event) {
+        var c = Shape.Circle(event.point, 20);
+        c.fillColor = 'green';
+    };
+
+    paper.view.draw();
+
+});
 
    
 // adapt from Learning Javascript Ethan Brown Chapter 1
