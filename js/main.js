d7c940737c9a40ad5c5037aa23c9f3597efcc877
EllenKC01 <ellen.k.cochran@gmail.com>
2021-09-12 23:50:01 -0400

    homework edits

--- js/main.js
+++ js/main.js
@@ -3,6 +3,18 @@
 /* 1.--------
  Write the JavaScript to display the current day and time in the following format and output it to the html page to the span with an id of "question-1"
 */
+var todayDate = new Date();
+
+var day = new Date().toLocaleString('en-us', {weekday:'long'})
+var time = new Date().toLocaleTimeString();
+console.log(day);
+now = "Today is " + day + ".";
+console.log(now);
+console.log(time);
+currentTime = "Current time : " + time;
+
+var todayIs = document.getElementById("answer1").innerHTML = now + "<br />" + currentTime;
+
 
 //adapt from https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date; https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat
 
@@ -12,7 +24,16 @@
 
 */
 
+var datestring1 = "0" + (todayDate.getMonth()+1) + "-" + todayDate.getDate() + "-" + todayDate.getFullYear();
+console.log(datestring1);
 
+var datestring2 = "0" + (todayDate.getMonth()+1) + "/" + todayDate.getDate() + "/" + todayDate.getFullYear();
+console.log(datestring2);
+
+var datestring3 = todayDate.getDate() + "/" + "0" + (todayDate.getMonth()+1) + "/" + todayDate.getFullYear();
+console.log(datestring3);
+
+var dateList = document.getElementById("answer2").innerHTML = datestring1 + "<br />" + datestring2 + "<br />" + datestring3;
 
 // adapt from https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date
 
@@ -21,15 +42,36 @@
 
 */
 
-
+function myFunction() {
+    var code = document.getElementById("body").innerHTML;
+    document.getElementById("answer3").innerText = code;
+}
 
 /* 4.--------
 
 */
 //
-   
 
-   
+paper.install(window);
+paper.setup(document.getElementById('main-canvas'));
+
+var pen = new Tool();
+
+var c = Shape.Circle(200, 200, 80);
+c.fillColor = 'black';
+var text = new PointText(200, 200);
+text.justification = 'center';
+text.fillColor = 'white';
+text.fontSize = 20;
+text.content = 'hello world';
+
+tool.onMouseDown = function(event) {
+    var c = Shape.Circle(event.point, 20);
+    c.fillColor = 'green';
+};
+
+paper.view.draw();
+
 
    
 // adapt from Learning Javascript Ethan Brown Chapter 1
